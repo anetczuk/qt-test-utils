290a3a0a243bac82df96231446f9845e3f9c6fd7
anetczuk <anetczuk@o2.pl>
2019-04-29 18:27:04 +0200

    Update scripts

--- make.sh
+++ make.sh
@@ -10,15 +10,28 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $SCRIPT_DIR/env.sh
 
 
+
+rebuild=0
+
+for i in "$@"; do
+case $i in
+    --rebuild)      rebuild=1
+                    shift                   # past argument with no value
+                    ;;
+    *)              ;;                      # unknown option
+esac
+done
+
+
+
 echo "Params: $@"
 
 
-PROJECT_DIR=$SCRIPT_DIR
-BUILD_DIR=$SCRIPT_DIR/build
-
-
-RELEASE_DIR=$BUILD_DIR/release
-DEBUG_DIR=$BUILD_DIR/debug
+if [ $rebuild -ne 0 ]; then
+    echo "Cleaning"
+    $SCRIPT_DIR/clean.sh
+    $SCRIPT_DIR/configure.sh
+fi
 
 
 cd $RELEASE_DIR
